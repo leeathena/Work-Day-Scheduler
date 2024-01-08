e0a6d20748b266c5a517d14cc3204787f9f75652
athena lee <hoyan.la@gmail.com>
2024-01-09 03:34:14 +0800

    Added save button fucntionality

--- starter/script.js
+++ starter/script.js
@@ -15,32 +15,45 @@ var currentHour = dayjs().hour();
 console.log(`current hour: ${currentHour}`)
 
 // Array for 9-17 time blocks
-var calendarTime = [0,9, 10, 11, 12, 13, 14, 15, 16, 17];
+var calendarTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 // Loop through each time block
 calendarTime.forEach(function(time) {
   // Select the corresponding event box by ID
   var eventBox = document.getElementById(time.toString());
+  var inputElement = document.querySelectorAll(".form-control")
 
-  // Check and apply color coding
+  // Check and apply color coding and block input
   if (currentHour > time) {
-    eventBox.classList.add('grey-box'); // Past
+    eventBox.classList.add('grey-box');
+    inputElement.disabled = true; // Past
   } else if (currentHour === time) {
-    eventBox.classList.add('red-box'); // Present
+    eventBox.classList.add('red-box');
+    inputElement.disabled = true; // Present
   } else {
-    eventBox.classList.add('green-box'); // Future
+    eventBox.classList.add('green-box');
+    inputElement.disabled = false; // Future
   }
 });
 
-
-//enable user input for present and future blocks
-
 //save the event in local sotrage when the save button is clicked; uniquely identify each time block so that the corresponding event data is correctly associated with it
 
-// saveButton.addEventListener('click', function() {
-
-
-//save button design
+saveBtns.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+    // Assuming the ID of the button is in the format 'save-XX'
+    var hour = this.id.split('-')[1];
+    
+    // Now get the input field corresponding to this hour
+    var inputElement = document.getElementById(hour);
+    
+    if (inputElement) {
+      var userInput = inputElement.value;
+      localStorage.setItem("eventDetail-" + hour, userInput);
+      console.log("Saved event for " + hour + ": " + userInput);
+    } else {
+      console.error("No input element found for hour: " + hour);
+    }
+  });
+});
 
 //Persist events between refreshes of a page
-//Page Load Event:check local storage for any saved events and populate the time blocks accordingly
